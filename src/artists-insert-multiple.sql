20805b284b7aa09d2aff9839033d0d1d1cf38250
Joseph Spencer <104227087+Jarseph@users.noreply.github.com>
2022-07-25 15:28:58 -0700

    Add files via upload

---
+++ src/artists-insert-multiple.sql
@@ -0,0 +1,7 @@
+INSERT INTO artists
+  (artist_name, birthday, birthplace, is_alive)
+  VALUES 
+  ('Wassily Kandinsky', '1866-12-16', 'Moscow, Russia', false),
+  ('Billy', '1865-12-16', 'Paris, France', false),
+  ('Kowalsky', '1864-12-16', 'Berlin, Germany', false),
+  ('Christmas', '1863-12-16', 'Toronto, Canada', false);
